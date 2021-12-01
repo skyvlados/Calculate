3d5386349bc31db352bb9dd432e546d37d3335c5
skyvlados <vladislav.repin@skyeng.ru>
2021-12-01 21:26:54 +0300

    init commit

---
+++ src/main/java/pro/sky/Calculate2/Calculate2Application.java
@@ -0,0 +1,13 @@
+package pro.sky.Calculate2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Calculate2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Calculate2Application.class, args);
+	}
+
+}
